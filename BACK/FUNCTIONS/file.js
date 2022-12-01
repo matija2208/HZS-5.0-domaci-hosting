a9c4ba027899fd09f148698d1ac6395318cdb8a4
matija cetrovic <matijacetrovic@gmail.com>
2022-12-01 15:40:06 +0100

    b

--- BACK/FUNCTIONS/file.js
+++ BACK/FUNCTIONS/file.js
@@ -1,11 +1,14 @@
 async function getPocetna(req,res)
 {
+    console.log("get pocetna");
     res.sendFile("pocetna.html");
+
 }
 
 async function get(req,res)
 {
     var file=req.params.file;
+    console.log("get "+file);
     res.sendFile(file);
 }
 
